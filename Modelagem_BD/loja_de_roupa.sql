d605171bf302ff53f88b432df72956c3ffb9139f
diego <136401880+DKB-17@users.noreply.github.com>
2024-05-19 16:26:48 -0300

    atualizacao do banco

--- Modelagem_BD/loja_de_roupa.sql
+++ Modelagem_BD/loja_de_roupa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/05/2024 às 21:19
+-- Tempo de geração: 19/05/2024 às 21:25
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 

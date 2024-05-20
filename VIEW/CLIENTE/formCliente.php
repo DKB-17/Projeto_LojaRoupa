7e385b1a3215cd289451d1110ab2b76e39f0c52d
diego <136401880+DKB-17@users.noreply.github.com>
2024-05-20 00:55:57 -0300

    feat: criar conta cliente

---
+++ VIEW/CLIENTE/formCliente.php
@@ -0,0 +1,79 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="pt-br" data-bs-theme="auto">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.122.0">
+  <title>Cadastro cliente</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <link href="sign-in.css" rel="stylesheet">
+</head>
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+  
+  <main class="form-signin w-100 m-auto">
+    <form>
+      
+      <div class="d-flex justify-content-center">
+      <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      </div>
+      <h1 class="h3 mb-3 fw-normal">Criar conta</h1>
+
+      <div class="grid gap-0 row-gap-3">
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Nome</label>
+      </div>
+
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">CPF:</label>
+      </div>
+
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Telefone</label>
+      </div>
+
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Data nascimento</label>
+      </div>
+
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Cidade</label>
+      </div>
+
+      <div class="form-floating p-2 g-col-6">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email</label>
+      </div>
+      <div class="form-floating p-2 g-col-6">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Senha</label>
+      </div>
+    </div>
+
+      <button class="btn btn-primary w-100 py-2" type="submit">Criar</button>
+    </form>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>

f1073126103b254ca21a6642e532669b53fe51ea
Manuela Dias <127984332+madiasm@users.noreply.github.com>
2024-06-10 21:07:46 -0300

    criação cabeçalho e footer

---
+++ VIEW/cabecalho.php
@@ -0,0 +1,75 @@
+<header>
+        <nav class="navbar fixed-top bg-light text-dark">
+            <div class="container-fluid">
+                <img class="ms-3 mt-2" src="./img/logobeyou.png" alt="a" width = "150" height = "75">
+
+
+                <div>
+
+                    <button class="navbar-toggler ms-3" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                </div>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Be You</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contato</a>
+                            </li>
+
+                            <li>
+                                <?php
+                                    if(isset($_COOKIE['autorizado']) && $_COOKIE['autorizado'] == 'true'){
+                                        echo '<li class="nav-item"><a class="nav-link active" aria-current="page" href="./logout.php">Logout</a></li>';
+                                        echo '<li class="nav-item"><a class="nav-link active" aria-current="page" href="./logout.php">Cadastrar Produto</a></li>';
+                                        
+                            
+                                    }else{
+                                        echo '<li class="nav-item"><a class="nav-link active" aria-current="page" href="./login.php">Login</a></li>';
+                                        
+                                    }
+                                ?>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Peças
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Masculina</a></li>
+                                    <li><a class="dropdown-item" href="#">Feminina</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Carrinho</a></li>
+                                </ul>
+
+                            </li>
+
+
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+
+    </header>
--- VIEW/index.php
+++ VIEW/index.php
@@ -10,111 +10,31 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar fixed-top bg-light text-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Be You</a>
+    <?php
+        include "./cabecalho.php"; 
+    ?>
 
-
-                <div>
-                    <?php
-                        if(isset($_COOKIE['autorizado']) && $_COOKIE['autorizado'] == 'true'){
-                            echo '<a href="./sair.php"><button type="button" class="btn btn-primary">Sair</button></a>';
-                            
-                        }else{
-                            echo '<a href="./login.php"><button type="button" class="btn btn-primary">Entrar</button></a>';
-                            
-                        }
-                    
-                    ?>
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                </div>
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Be You</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contato</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Peças
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Masculina</a></li>
-                                    <li><a class="dropdown-item" href="#">Feminina</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Carrinho</a></li>
-
-                                </ul>
-
-                            </li>
-
-
-                        </ul>
-                        <form class="d-flex mt-3" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-
-
-    </header>
+    
+    
     <main>
         <br>
         <br>
         <br>
     </main>
 
-    <footer
-        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top p-3 mb-2 bg-light text-dark">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
-        </div>
+    
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
-                        <use xlink:href="#twitter"></use>
-                    </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
-                        <use xlink:href="#instagram"></use>
-                    </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24">
-                        <use xlink:href="#facebook"></use>
-                    </svg></a></li>
-        </ul>
-    </footer>
 
+    
+
+    <?php
+        include "./footer.php"; 
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
 </body>
 
 

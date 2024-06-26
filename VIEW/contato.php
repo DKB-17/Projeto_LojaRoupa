14b432c00be99872de6df40bc9d39e4fd24cb61d
diego <136401880+DKB-17@users.noreply.github.com>
2024-06-25 22:18:20 -0300

    feat: Validacao de entrada

--- VIEW/contato.php
+++ VIEW/contato.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Be You - Roupas</title>
+    <title>Be You - Contato</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -16,7 +16,7 @@
     
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
-
+    <link href="./checkout.css" rel="stylesheet">
 </head>
 
 <body>
@@ -77,5 +77,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+    <script src="./checkout.js"></script>
 </body>
 </html>
